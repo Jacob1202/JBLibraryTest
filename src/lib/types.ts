80fd98a1c1f67a022e02bdb8ea63ac2355476e68
Jakub Biniek <kuba.biniek01@gmail.com>
2023-08-28 18:08:26 +0200

    basic blog setup with remark, slug md files

---
+++ src/lib/types.ts
@@ -0,0 +1,13 @@
+
+
+type PostType = {
+  id: number
+  slug: string
+  title: string
+  date: string
+  author: string
+  tags: string[]
+  content: string
+}
+
+export default PostType
--- src/pages/ml/index.tsx
+++ src/pages/ml/index.tsx
@@ -1,12 +1,45 @@
-import Article from "~/components/Article";
-import { dataML } from "~/content/ml";
+import Head from "next/head";
+import { getAllPosts } from "~/lib/articles";
+import type PostType from "~/lib/types";
 
-export default function Dotnet() {
+type Props = {
+  allPosts: PostType[];
+};
+
+export default function MachineLearning({ allPosts }: Props) {
   return (
     <>
+      <Head>
+        <title>Machine Learning | Jakub Biniek</title>
+        <meta
+          name="description"
+          content="This is a documentation/library of my growth as a software developer"
+        />
+      </Head>
       <div className="mt-16 flex min-h-screen flex-col items-start justify-start gap-8 px-4 sm:px-0">
-        <Article title={"Machine Learning Library"} data={dataML} />
+        <h1 className="text-2xl font-semibold sm:text-4xl">
+          Machine Learning Library
+        </h1>
+        {allPosts.map((item) => (
+          <div key={item.id}>{item.tags}</div>
+        ))}
       </div>
     </>
   );
 }
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    "title",
+    "date",
+    "slug",
+    "author",
+    "tags",
+    "excerpt",
+  ]);
+
+  return {
+    props: { allPosts },
+  };
+};
